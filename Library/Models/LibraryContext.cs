f83b95c72d2f346f60194b48ec08150e61f1e473
Anton <aemelia2004@gmail.com>
2026-03-26 13:19:15 +0500

    Форма регистрации

---
+++ Library/Models/LibraryContext.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Models;
+
+public partial class LibraryContext : DbContext
+{
+    public LibraryContext()
+    {
+    }
+
+    public LibraryContext(DbContextOptions<LibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<Loan> Loans { get; set; }
+
+    public virtual DbSet<PublishingHouse> PublishingHouses { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<Status> Statuses { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Username=postgres;Database=library;Password=1111");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("authors_pkey");
+
+            entity.ToTable("authors");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("books_pkey");
+
+            entity.ToTable("books");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Annotation).HasColumnName("annotation");
+            entity.Property(e => e.AvailableCopies).HasColumnName("available_ copies");
+            entity.Property(e => e.IdAuthor).HasColumnName("id_author");
+            entity.Property(e => e.IdGenre).HasColumnName("id_genre");
+            entity.Property(e => e.IdPublishingHouse).HasColumnName("id_publishing_house");
+            entity.Property(e => e.Isbn).HasColumnName("isbn");
+            entity.Property(e => e.Pages).HasColumnName("pages");
+            entity.Property(e => e.TotalCopies).HasColumnName("total_copies");
+            entity.Property(e => e.Year).HasColumnName("year");
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.IdAuthor)
+                .HasConstraintName("books_id_author_fkey");
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Books)
+                .HasForeignKey(d => d.IdGenre)
+                .HasConstraintName("books_id_genre_fkey");
+
+            entity.HasOne(d => d.PublishingHouse).WithMany(p => p.Books)
+                .HasForeignKey(d => d.IdPublishingHouse)
+                .HasConstraintName("books_id_publishing_house_fkey");
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("genre_pkey");
+
+            entity.ToTable("genres");
+
+            entity.Property(e => e.Id)
+                .HasDefaultValueSql("nextval('genre_id_seq'::regclass)")
+                .HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Loan>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("loans_pkey");
+
+            entity.ToTable("loans");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateIssue).HasColumnName("date_issue");
+            entity.Property(e => e.IdBook).HasColumnName("id_book");
+            entity.Property(e => e.IdStatus).HasColumnName("id_status");
+            entity.Property(e => e.IdUser).HasColumnName("id_user");
+            entity.Property(e => e.PlannedReturnDate).HasColumnName("planned_return_date");
+            entity.Property(e => e.ReturnDate).HasColumnName("return_date");
+
+            entity.HasOne(d => d.Book).WithMany(p => p.Loans)
+                .HasForeignKey(d => d.IdBook)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("loans_id_book_fkey");
+
+            entity.HasOne(d => d.Status).WithMany(p => p.Loans)
+                .HasForeignKey(d => d.IdStatus)
+                .HasConstraintName("loans_id_status_fkey");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Loans)
+                .HasForeignKey(d => d.IdUser)
+                .HasConstraintName("loans_id_user_fkey");
+        });
+
+        modelBuilder.Entity<PublishingHouse>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("publishing_houses_pkey");
+
+            entity.ToTable("publishing_houses");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("roles_pkey");
+
+            entity.ToTable("roles");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Status>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("statuses_pkey");
+
+            entity.ToTable("statuses");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.FirstName).HasColumnName("first_name");
+            entity.Property(e => e.IdRole).HasColumnName("id_role");
+            entity.Property(e => e.Login).HasColumnName("login");
+            entity.Property(e => e.Name).HasColumnName("name");
+            entity.Property(e => e.Password).HasColumnName("password");
+            entity.Property(e => e.Patronymic).HasColumnName("patronymic");
+            entity.Property(e => e.Ticket).HasColumnName("ticket");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Users)
+                .HasForeignKey(d => d.IdRole)
+                .HasConstraintName("users_id_role_fkey");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Library/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public int IdRole { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Patronymic { get; set; } = null!;
+
+    public string Ticket { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual ICollection<Loan> Loans { get; set; } = new List<Loan>();
+}
